89500c0f93487596a5b622a793004d2a9b9bf83f
nguyentrungduc2003 <125373547+nguyentrungduc2003@users.noreply.github.com>
2023-04-24 09:54:42 +0700

    updae code

---
+++ baithuchanhlaB3/src/HINHHOC/HinhTron.java
@@ -0,0 +1,22 @@
+package HINHHOC;
+import java.util.Scanner;
+
+public class HinhTron extends HinhHoc {
+    public float bankinh;
+
+    public HinhTron(){
+        ten = "hinh tron";
+    }
+    public void nhapbankinh(){
+        System.out.println("ban kinh =");
+        Scanner sc = new Scanner(System.in);
+        bankinh = sc.nextFloat();
+    }
+    public void tinhchuvi(){
+        chuvi = 2* PI * bankinh;
+    }
+    public void tinhdientich(){
+        dientich = PI * bankinh * bankinh;
+    }
+}
+
---
+++ baithuchanhlaB3/src/HINHHOC/HinhVuong.java
@@ -0,0 +1,14 @@
+package HINHHOC;
+import java.util.Scanner;
+
+public class HinhVuong extends HinhChuNhat{
+    public HinhVuong(){
+        ten = "hinh vuong";
+    }
+    public void nhapcanh(){
+        System.out.println("canh =");
+        Scanner scanner = new Scanner(System.in);
+        dai = rong = scanner.nextFloat();
+    }
+}
+
